341891e56f03f124aaff69017db3cbbea339e65b
AngelIdk666 <83241924+AngelIdk666@users.noreply.github.com>
2024-11-23 12:37:01 -0600

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,13 +1,13 @@
-import mysql from "mysql2/promise";
+const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "", // Coloca tu contraseña de MySQL
-  database: "CrudNodejs",
+  host: process.env.DB_HOST || "database", // Nombre del servicio MySQL
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "root", // Contraseña configurada en docker-compose.yml
+  database: process.env.DB_NAME || "CrudNodejs",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
 });
 
-export default pool;
+module.exports = pool;
