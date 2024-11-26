df061e52be6365cc028184d6fa711999ab857a94
AngelIdk666 <83241924+AngelIdk666@users.noreply.github.com>
2024-11-26 15:20:50 -0600

    Update estudiantes.sql

--- BD/estudiantes.sql
+++ BD/estudiantes.sql
@@ -9,5 +9,9 @@ CREATE TABLE `estudiantes` (
   PRIMARY KEY (`id_estudiante`)
 ) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-
-/INSERT INTO `estudiantes` VALUES (35,'Hola','53453go@gmail.com','PHP','2023-12-07'),(36,'Carlos x','667567@gmail.com','Python','2023-12-07'),(37,'nuevo','n@gmail.com','Python','2023-12-07'),(39,'nuevooo','n@gmail.com','PHP','2023-12-07'),(40,'nuevodsadasd','1eqwe12311@gmail.com','NodeJS','2023-12-07');
+INSERT INTO `estudiantes` VALUES 
+(35,'Hola','53453go@gmail.com','PHP','2023-12-07'),
+(36,'Carlos x','667567@gmail.com','Python','2023-12-07'),
+(37,'nuevo','n@gmail.com','Python','2023-12-07'),
+(39,'nuevooo','n@gmail.com','PHP','2023-12-07'),
+(40,'nuevodsadasd','1eqwe12311@gmail.com','NodeJS','2023-12-07');
